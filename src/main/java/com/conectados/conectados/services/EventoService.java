978823076a9a15746721e327f0a9bf2a27cd8f05
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-24 12:17:31 -0300

    :sparkles: feat: Implement EventoService for managing Evento entity operations

---
+++ src/main/java/com/conectados/conectados/services/EventoService.java
@@ -0,0 +1,40 @@
+package com.conectados.conectados.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.conectados.conectados.domain.model.Evento;
+import com.conectados.conectados.domain.repository.EventoRepository;
+
+@Service
+public class EventoService {
+
+    @Autowired
+    private EventoRepository eventoRepository;
+
+    public Optional<Evento> buscarEventoPorId(Long id) {
+        return eventoRepository.findById(id);
+    }
+
+    public Evento cadastrarEvento(Evento evento) {
+        return eventoRepository.save(evento);
+    }
+
+    public Optional<List<Evento>> buscarTodosEventos() {
+        List<Evento> eventos = eventoRepository.findAll();
+        if (eventos.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(eventos);
+    }
+
+    public Evento DeletarEvento(Long id) {
+        Evento evento = eventoRepository.findById(id).orElseThrow(() -> new RuntimeException("Evento não encontrado"));
+        eventoRepository.delete(evento);
+        return evento;
+    }
+    
+}
