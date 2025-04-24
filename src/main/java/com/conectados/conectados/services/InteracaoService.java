ee7666bc6c332da3d8fc49cad6dc2cf1908f3b8f
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-24 12:20:44 -0300

    :sparkles: feat: Implement InteracaoService for managing Interacao entity operations

---
+++ src/main/java/com/conectados/conectados/services/InteracaoService.java
@@ -0,0 +1,42 @@
+package com.conectados.conectados.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.conectados.conectados.domain.model.Interacao;
+import com.conectados.conectados.domain.repository.InteracaoRepository;
+
+@Service
+public class InteracaoService {
+
+    @Autowired
+    private InteracaoRepository interacaoRepository;
+
+    public Optional<List<Interacao>> buscarTodasInteracoes() {
+        List<Interacao> interacoes = interacaoRepository.findAll();
+        if (interacoes.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(interacoes);
+    }
+
+    public Optional<Interacao> buscarInteracaoPorId(Long id) {
+        return interacaoRepository.findById(id);
+    }
+
+    public Interacao cadastrarInteracao(Interacao interacao) {
+        return interacaoRepository.save(interacao);
+    }
+
+    public Interacao atualizarInteracao(Interacao interacao) {
+        return interacaoRepository.save(interacao);
+    }
+
+    public void deletarInteracao(Long id) {
+        interacaoRepository.deleteById(id);
+    }
+
+}
