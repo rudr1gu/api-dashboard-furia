3f9d90999b90426ff318e0aeab4dfca467ef5373
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-24 11:03:27 -0300

    :sparkles: feat: Create Interacao entity with attributes for id and tipo

---
+++ src/main/java/com/conectados/conectados/domain/model/Interacao.java
@@ -0,0 +1,22 @@
+package com.conectados.conectados.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_interacoes")
+public class Interacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tipo; 
+}
