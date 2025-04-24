5841c58c788f1b398ea84e5e41b5ddda62c1ac75
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-23 22:28:34 -0300

    :sparkles: feat: Add Nivel entity with attributes for name and associated Usuario

---
+++ src/main/java/com/conectados/conectados/domain/model/Nivel.java
@@ -0,0 +1,33 @@
+package com.conectados.conectados.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_nivel")
+public class Nivel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 10, max = 150)
+    private String nome;
+
+    @ManyToOne
+    @JsonIgnoreProperties("nivel")
+    private Usuario usuario;
+
+}
