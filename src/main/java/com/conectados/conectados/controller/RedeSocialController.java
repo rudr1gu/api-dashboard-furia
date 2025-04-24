a8d2637e48868aaba1c2bb201a0b2cdc801c2de4
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-24 15:04:55 -0300

    :sparkles: feat: Add RedeSocialController for managing Rede Social operations

---
+++ src/main/java/com/conectados/conectados/controller/RedeSocialController.java
@@ -0,0 +1,64 @@
+package com.conectados.conectados.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.conectados.conectados.domain.model.RedeSocial;
+import com.conectados.conectados.services.RedeSocialService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/redes-sociais")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class RedeSocialController {
+
+    @Autowired
+    private RedeSocialService redeSocialService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<RedeSocial>> getAllRedesSociais() {
+        return ResponseEntity.ok(redeSocialService.buscarTodasRedesSociais().orElse(null));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RedeSocial> getById(@PathVariable Long id) {
+        return redeSocialService.buscarRedeSocialPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<RedeSocial> cadastrarRedeSocial(@RequestBody RedeSocial redeSocial) {
+        return ResponseEntity.ok(redeSocialService.cadastrarRedeSocial(redeSocial));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<RedeSocial> atualizarRedeSocial(@PathVariable Long id, @RequestBody RedeSocial redeSocial) {
+        redeSocial.setId(id);
+        return ResponseEntity.ok(redeSocialService.atualizarRedeSocial(redeSocial));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deletarRedeSocial(@PathVariable Long id) {
+        if (redeSocialService.buscarRedeSocialPorId(id).isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Rede Social não encontrada");
+        }
+        redeSocialService.deletarRedeSocial(id);
+    }
+}
